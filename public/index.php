ebdcfc237145867c6d08261987843daca2bdd3ce
s.ichitani <serina.ichitani@freeeks.jp>
2025-05-26 07:25:43 +0900

    [clean][add]refs #002 84. Split UI Into Partials

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require '../helpers.php';
 // require '../views/home.view.php';
-require basePath('views/home.view.php');
+// require basePath('views/home.view.php');
+loadView('home');
 
 ?>
