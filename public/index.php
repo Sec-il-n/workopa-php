a3b0010133c4dcfdd57a7944b7b37f1916aea54a
s.ichitani <serina.ichitani@freeeks.jp>
2025-06-01 07:51:00 +0900

    [add][update]refs #004 86. Create a VERY Basic Router

--- public/index.php
+++ public/index.php
@@ -5,16 +5,18 @@ require '../helpers.php';
 // loadView('home');
 
 $routes = [
+    // '/workopia/public/' => 'controllers/home.php',
     '/' => 'controllers/home.php',
     '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
+    '/listings/create' => 'controllers/listings/create/create.php',
     '404' => 'controllers/error/404.php'
 ];
 
 
 $uri = $_SERVER['REQUEST_URI'];
 // inspectAndDie($uri);
-// inspect($uri);
+inspect($uri);
+inspect($routes[$uri]);
 if (array_key_exists($uri, $routes)) {
     require(basePath($routes[$uri]));
 } else {
